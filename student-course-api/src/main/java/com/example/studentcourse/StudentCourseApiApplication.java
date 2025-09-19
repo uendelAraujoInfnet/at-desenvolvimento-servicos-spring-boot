4b479a154375e562c6c7c25789b26cc6e1483f57
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-09-19 18:16:42 -0400

    Adicionado o projeto basico gerado pelo Initializr

---
+++ student-course-api/src/main/java/com/example/studentcourse/StudentCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseApiApplication.class, args);
+	}
+
+}
