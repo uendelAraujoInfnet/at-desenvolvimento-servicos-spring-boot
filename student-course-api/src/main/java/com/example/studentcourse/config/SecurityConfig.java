592a26eae6120fe5881197a8a6ec5c2b6e9dc6ca
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-09-20 14:53:46 -0400

    adicionada a classe de securityconfig porem sem password encoded para facilitar o trabalho de testes

---
+++ student-course-api/src/main/java/com/example/studentcourse/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.studentcourse.config;
+
+import com.example.studentcourse.model.Professor;
+import com.example.studentcourse.repository.ProfessorRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final ProfessorRepository professorRepository;
+
+    public SecurityConfig(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            Professor professor = professorRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("Professor not found ( Professor não encontrado)"));
+            return User.withUsername(professor.getUsername())
+                    .password(professor.getPassword())
+                    .roles("PROFESSOR")
+                    .build();
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->
+                auth.requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/api/professors").permitAll()
+                        .anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+
+        http.headers(headers -> headers
+                .frameOptions(frame -> frame.sameOrigin()));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
