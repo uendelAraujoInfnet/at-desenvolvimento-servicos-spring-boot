2c29cbcf383a5eca0d6fb498fe28556ec827e5bd
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-09-23 19:50:36 -0400

    Configuraçcoes para correcoes de BUG com o frontend - front nao consegue reconhecer backend apos deploy

--- student-course-api/src/main/java/com/example/studentcourse/config/CorsConfig.java
+++ student-course-api/src/main/java/com/example/studentcourse/config/CorsConfig.java
@@ -1,4 +1,20 @@
 package com.example.studentcourse.config;
 
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://at-desenvolvimento-servicos-spring.vercel.app/", "http://localhost:5173")
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
